f2bc7ce114f6c0243627e4a2d039fb736332571f
irmanfreestyle <=>
2022-05-15 14:12:23 +0700

    Create simple utility classes (grid system, text align, spacing)

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,17 @@
 <template>
+  <div class="row">
+    <div class="bg-primary col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <h4>Content</h4>
+    </div>
+
+    <div class="bg-primary col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <h4>Content</h4>
+    </div>
+
+    <div class="bg-primary col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <h4>Content</h4>
+    </div>
+  </div>
 </template>
 
 <script>
